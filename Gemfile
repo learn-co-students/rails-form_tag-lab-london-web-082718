93a3b4bdd392d6d266bcac8e31da2c3f23753391
github username <github email address>
2018-09-24 12:37:48 +0100

    Done.

--- Gemfile
+++ Gemfile
@@ -23,8 +23,8 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
-
 group :development, :test do
+
   gem 'byebug'
   gem 'spring'
   gem 'rspec-rails'
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,23 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
+  end
+
+  def new
   end
 
   def show
     @student = Student.find(params[:id])
   end
+
+  def create
+    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    #  id for these params needs to be "first_name" and "last_name" - check on browser, inspect
+    # element to see what the id is - good tip - Ed showed
+
+    redirect_to @student  # Rails knows we are trying to redirect_to the student instance we just created
+  end
+
 
 end
